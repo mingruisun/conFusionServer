87d113d02bcb2669e8612353271740b00c47cca2
Cary Sun <carysun@Carys-iMac-Pro.local>
2018-02-10 17:02:19 -0800

    added session support

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -37,14 +39,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
 
 function auth(req, res, next)
 {
     // check if a cookie exists, if not, prompt user to login, otherwise check the cookie and move forward.
-    console.log(req.signedCookies);
+    // console.log(req.signedCookies);
+    console.log(req.session);
 
-    if (!req.signedCookies.user) {
+    if (!req.session.user) {
         var authHeader = req.headers.authorization;
 
         if (!authHeader) {
@@ -59,7 +69,8 @@ function auth(req, res, next)
         var password = auth[1];
 
         if (username === 'admin' && password === 'password') {
-            res.cookie('user', 'admin', {signed: true});
+            // res.cookie('user', 'admin', {signed: true});
+            req.session.user = 'admin';
             next();
         }
         else {
@@ -70,7 +81,7 @@ function auth(req, res, next)
         }
     }
     else {
-        if (req.signedCookies.user === 'admin')
+        if (req.session.user === 'admin')
         {
             next();
         }
